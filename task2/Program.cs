e0b9dbdf2de6ae2b20f25fc7bb49c56c6e68aecb
Aleksandra <kharchenko.aleksandra@yandex.ru>
2022-09-15 14:23:02 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+// стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+int size = 8;
+int[] array = FillArray(size);
+System.Console.WriteLine("[{0}]", string.Join(",", array));
+System.Console.WriteLine($"Сумма нечетных чисел в массиве равна: {GetSumNechet(array)}");
+int[] FillArray(int size)
+{
+    int [] array =new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(1,20);   
+    }
+    return array;
+}
+
+int GetSumNechet(int[]array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 1)
+        {
+            sum+=array[i];
+        }
+    }
+    return sum;
+}
+
+
